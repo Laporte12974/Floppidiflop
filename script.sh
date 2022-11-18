2efc210458a789be6c661203dad9efe9a3c3082b
Laporte <theguywholivesinacomputer@gmail.com>
2022-11-18 19:04:12 +0100

    The real thing

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
-#!/run/current-system/sw/bin/bash
+#!/bin/bash
 while true
 do
-for f in ./Downloads/*
+for f in ./Frames/*
 do
 	cat $f
-	sleep 0.0475
+	sleep 0.05
 	clear
-	sleep 0.0025
 done
 done
